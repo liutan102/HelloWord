7ca4b3cfd08be4f85b3cac6b8162d548844945a0
liutan102 <386434102@qq.com>
2020-03-21 18:02:34 +0800

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,31 @@
+# Python 学习
+# 汉诺塔
+a = "1"
+b = "2"
+c = "3"
+def hannuo(a,b,c,n):
+    if n == 1:
+        print("{} @@@> {}".format(a,c))
+        return None
+    if n == 2:
+        print("{} @@@> {}".format(a,b))
+        print("{} @@@> {}".format(a,c))
+        print("{} @@@> {}".format(b,c))
+        return None
+    hannuo(a,c,b,n-1)
+    print("{} @@@> {}".format(a,c))
+    hannuo(b,a,c,n-1)
+hannuo(a,b,c,5)
+
+hannuo(a,b,c,3)
+hannuo(a,b,c,1)
+hannuo(a,b,c,3)
+hannuo(a,b,c,1)
+hannuo(a,b,c,3)
+hannuo(a,b,c,1)
+hannuo(a,b,c,3)
+hannuo(a,b,c,1)
+hannuo(a,b,c,3)
+hannuo(a,b,c,1)
+
+
