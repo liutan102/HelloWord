49ad787f1d90725e290c59ca9ff78c49abca1883
liutan102 <386434102@qq.com>
2020-03-21 18:43:54 +0800

    阿萨啊啊啊啊啊啊

--- Demo.py
+++ Demo.py
@@ -21,11 +21,16 @@ hannuo(a,b,c,3)
 hannuo(a,b,c,1)
 hannuo(a,b,c,3)
 hannuo(a,b,c,1)
-hannuo(a,b,c,3)
-hannuo(a,b,c,1)
-hannuo(a,b,c,3)
-hannuo(a,b,c,1)
-hannuo(a,b,c,3)
-hannuo(a,b,c,1)
 
-
+def hannuo1(a,b,c,n):
+    if n == 1:
+        print("{} @@@> {}".format(a,c))
+        return None
+    if n == 2:
+        print("{} @@@> {}".format(a,b))
+        print("{} @@@> {}".format(a,c))
+        print("{} @@@> {}".format(b,c))
+        return None
+    hannuo1(a,c,b,n-1)
+    print("{} @@@> {}".format(a,c))
+    hannuo(b,a,c,n-1)
